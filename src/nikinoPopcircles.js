5c01f98237bbe49b765e87224b9509a4d0b9b9d7
broAir <andrey.bobrov.dev@gmail.com>
2016-04-11 11:07:21 +0800

    smooth animation for expanded popcircles

--- src/nikinoPopcircles.js
+++ src/nikinoPopcircles.js
@@ -9,29 +9,65 @@
             var children = $(".popcircle-container").find(".popcircle-children").children();
             var angle = Math.PI * 2 / children.length;
 
-            var parent = $(".popcircle-container").find(".popcircle");
-            var parentRadius = parent.outerWidth(true) / 2;
-
+            var $parent = $(".popcircle-container").find(".popcircle");
+            var parentRadius = $parent.outerWidth(true) / 2;
+            
+            $parent.attr("data-radius", parentRadius);
+            
             children.each(function (index, el) {
-                var thisRad = $(el).width() / 2;
+                var $el = $(el);
+                
+                var thisRad = $el.width() / 2;
                 var num = index * angle;
-                var posX = parentRadius + (Math.cos(num)) * (parentRadius) - thisRad;
-                var posY = parentRadius + (-Math.sin(num)) * (parentRadius) - thisRad;
-
-                $(el).css({top: posY, left: posX});
+                
+                var posX = parentRadius + (Math.cos(num)) * (parentRadius/2) - thisRad;
+                var posY = parentRadius + (-Math.sin(num)) * (parentRadius/2) - thisRad;
+                
+                $el.attr("data-x-rad-increment", Math.round(Math.cos(num) * parentRadius/2));
+                $el.attr("data-y-rad-increment", Math.round(-Math.sin(num) * parentRadius/2));
+                
+                $el.css({top: posY, left: posX});
             });
         },
 
+        _expandChildren:function(){
+             var children = $(".popcircle-container").find(".popcircle-children").children();
+             children.finish();
+             
+             children.each(function (index, el) {
+                var $el = $(el);
+                
+                var newTop = $el.position().top+ +$el.attr("data-y-rad-increment");
+                var newLeft = $el.position().left+ +$el.attr("data-x-rad-increment");
+                
+                $el.animate({top: newTop, left: newLeft, opacity:0.5}, "fast");
+             });
+        },
+        
+        _foldChildren:function(){
+            var children = $(".popcircle-container").find(".popcircle-children").children();
+             children.finish();
+             children.each(function (index, el) {
+                var $el = $(el);
+                
+                var newTop = $el.position().top- +$el.attr("data-y-rad-increment");
+                var newLeft = $el.position().left- +$el.attr("data-x-rad-increment");
+                
+                $el.animate({top: newTop, left: newLeft, opacity:0.0}, "fast");
+             });
+        },
+        
         _delegateEvents: function () {
+            var that = this;
             $(".popcircle-container").bind("mouseenter", function (e) {
                 var $this = $(this);
-                $this.find(".popcircle-child-free").switchClass("popcircle-child-free", "popcircle-child-active", "slow");
-            });
+                that._expandChildren();
+        });
 
             $(".popcircle-container").bind("mouseleave", function (e) {
                 var $this = $(this);
-                $this.find(".popcircle-child-active").switchClass("popcircle-child-active", "popcircle-child-free", "fast");
-            });
+                that._foldChildren();    
+        });
 
             $(".popcircle-container").draggable({
                 handle: ".popcircle",
